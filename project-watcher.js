6eeb5f87119b33c944c4325241d788662126776d
Minified <gora.pub@gmail.com>
2015-12-16 17:55:56 +0100

    watcher.js + listen for changes in options json

--- project-watcher.js
+++ project-watcher.js
@@ -12,70 +12,37 @@ console.log("|___|_  |  |_|_|_|_____|_|_|_|_| |_|___|___|                    ");
 console.log("    |___|                                                       ");
 console.log("");
 var fs = require("fs");
-var settings = {
-  module_path : null,
-  path : '.',
-  destination : '_PRODUCED',
-  babel : [],
-  babel_options : {},
-  copy : ["*"]
-}
-try{
-  let options = JSON.parse(fs.readFileSync('project-watcher-options.json'));
-  for(let opt in options){
-    settings[opt] = options[opt];
-  }
-}catch(e){
-  console.log(e)
-}
-if(settings.module_path)module.paths.push(settings.module_path);
-var babel = require("babel-core");
-var find = require('fs-finder');
+var watcher = require('./watcher.js');
 
-var thispath = fs.realpathSync(settings.path);
-console.log('THIS PATH: ',thispath);
-function logDate(){
-  console.log(Date());
-  return '';
-}
-function babelFile(f){
-  logDate();
-  console.log('DID BABEL: ',f);
-  babel.transformFile(f, settings.babel_options, function (err, result) {
-    // result; // => { code, map, ast }
-    if(err){
-      console.log(err);
-    }else{
-      fs.writeFile(settings.destination+f,result.code,{flag:'w+'},function(err){if(err)console.log(err);})
+var DEFAULT = {
+  settings : function(){
+    return {
+      module_path : null,
+      source_path : '.',
+      destination_path : '_PRODUCED',
+      babel : [],
+      babel_options : {},
+      copy : ["*"]
     }
-  });
+  }
 }
-function copyFile(f){
-  logDate();
-  console.log('DID COPY: ',f);
-  fs.readFile(f,(err,data)=>{
-    fs.writeFile(settings.destination+f,data,{flag:'w+'},(err)=>{if(err)console.log(err);})
-  })
-}
-function watchFile(f,actionForFile){
-  console.log('WATCHING: ',f);
-  fs.watchFile(f,function(curr,prev){
-    actionForFile(f);
-  }.bind(this))
-}
-for(let f of settings.babel){
-  babelFile(f);
-  watchFile(f,babelFile);
-}
-for(let f of settings.copy){
-  find.from('.').exclude(['node_modules']).findFiles(f,(files)=>{
-    // console.log(files);
-    for(let file of files){
-      file=file.replace(thispath,'').replace('/\//g','\\');
-      file = file[0]==='\\' ? file.slice(1) : file;
-      console.log('SET COPY FOR: ',file);
-      copyFile(file);
-      watchFile(file,copyFile);
+var w = null;
+function start(){
+  try{
+    w.clear();
+  }catch(e){}
+  var settings = DEFAULT.settings();
+  try{
+    let options = JSON.parse(fs.readFileSync('project-watcher-options.json'));
+    for(let opt in options){
+      settings[opt] = options[opt];
     }
-  })
+  }catch(e){
+    console.log(e)
+  }
+  w = new watcher.Watcher(settings);
 }
+start();
+fs.watchFile('project-watcher-options.json',(curr,prev)=>{
+  start();
+})
---
+++ watcher.js
@@ -0,0 +1,82 @@
+'use strict';
+
+class Watcher{
+  logDate(){
+    console.log(Date());
+    return '';
+  }
+  notifyErr(err){
+    if(!err){
+      return;
+    }
+    this.notifier.notify({
+      'title':'Project Watcher: ERROR',
+      'subtitle':err.toString(),
+      'message':err.toString(),
+      'wait':true
+    },function(error,response){
+      // console.log(error);
+      // console.log(response);
+    })
+  }
+  babelFile(f){
+    this.logDate();
+    console.log('DID BABEL: ',f);
+    this.babel.transformFile(f, this.settings.babel_options, function (err, result) {
+      // result; // => { code, map, ast }
+      if(err){
+        console.log(err);
+        this.notifyErr(err);
+      }else{
+        this.fs.writeFile(this.settings.destination_path+f,result.code,{flag:'w+'},(err)=>{if(err)console.log(err);this.notifyErr(err);})
+      }
+    }.bind(this));
+  }
+  copyFile(f){
+    this.logDate();
+    console.log('DID COPY: ',f);
+    this.fs.readFile(f,(err,data)=>{
+      this.fs.writeFile(this.settings.destination_path+f,data,{flag:'w+'},(err)=>{if(err){console.log(err);((err)=>{this.notifyErr(err);})(err);}})
+    })
+  }
+  watchFile(f,actionForFile){
+    console.log('WATCHING: ',f);
+    this.watchers.push({file:f});
+    this.fs.watchFile(f,function(curr,prev){
+      actionForFile(f);
+    }.bind(this))
+  }
+  constructor(settings){
+    this.watchers = [];
+    this.settings = settings;
+    if(this.settings.module_path)module.paths.push(this.settings.module_path);
+    this.fs = require('fs');
+    this.babel = require("babel-core");
+    this.find = require('fs-finder');
+    this.notifier = require('node-notifier');
+    this.thispath = this.fs.realpathSync(this.settings.source_path);
+    console.log('THIS PATH: ',this.thispath);
+    for(let f of this.settings.babel){
+      this.babelFile(f);
+      this.watchFile(f,this.babelFile);
+    }
+    for(let f of this.settings.copy){
+      this.find.from('.').exclude(['node_modules']).findFiles(f,(files)=>{
+        // console.log(files);
+        for(let file of files){
+          file=file.replace(this.thispath,'').replace('/\//g','\\');
+          file = file[0]==='\\' ? file.slice(1) : file;
+          console.log('SET COPY FOR: ',file);
+          this.copyFile(file);
+          this.watchFile(file,this.copyFile);
+        }
+      })
+    }
+  }
+  clear(){
+    for(let w of this.watchers){
+      this.fs.unwatchFile(w.file);
+    }
+  }
+}
+exports.Watcher = Watcher;
